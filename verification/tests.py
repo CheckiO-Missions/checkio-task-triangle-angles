32a1428b8f5a8d2c5606a56067c876954aad7a35
pcurzytek <curzytekp@gmail.com>
2017-08-01 23:55:11 -0400

    grammar corrections

--- verification/tests.py
+++ verification/tests.py
@@ -4,7 +4,7 @@ Keys for this will be categories' names.
 Each test is dict with
     "input" -- input data for user function
     "answer" -- your right answer
-    "explanation" -- not necessary key, it's using for additional info in animation.
+    "explanation" -- not necessary key, it's used for additional info in animation.
 """
 
 TESTS = {
